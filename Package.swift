1b7b9bfaa4c8be997135f774a9950aa443ff8f24
jungao <jungao@staff.weibo.com>
2025-04-30 19:25:33 +0800

    feature:使用独立的tree sitter

--- Package.swift
+++ Package.swift
@@ -9,19 +9,25 @@ let package = Package(
         .library(name: "TreeSitterDocument", targets: ["TreeSitterDocument"]),
     ],
     dependencies: [
+        .package(url: "https://gitee.com/bugtree/tree-sitter", branch: "spm"),
     ],
     targets: [
-        .target(name: "tree-sitter",
-                path: "tree-sitter/lib",
-                sources: ["src/lib.c"],
-                publicHeadersPath: "include",
-                cSettings: [.headerSearchPath("src/")]),
+        // .target(name: "tree-sitter",
+        //         path: "tree-sitter/lib",
+        //         sources: ["src/lib.c"],
+        //         publicHeadersPath: "include",
+        //         cSettings: [.headerSearchPath("src/")]),
         .target(name: "TestTreeSitterSwift",
                 path: "tree-sitter-swift",
                 sources: ["src/parser.c", "src/scanner.c"],
                 publicHeadersPath: "bindings/swift",
                 cSettings: [.headerSearchPath("src")]),
-        .target(name: "SwiftTreeSitter", dependencies: ["tree-sitter"]),
+        .target(
+            name: "SwiftTreeSitter", 
+            dependencies: [
+                    .product(name: "TreeSitter", package: "tree-sitter")
+                ]
+            ),
         .testTarget(name: "SwiftTreeSitterTests",
                     dependencies: ["SwiftTreeSitter", "TestTreeSitterSwift"]),
         .target(name: "TreeSitterDocument", dependencies: ["SwiftTreeSitter"]),
